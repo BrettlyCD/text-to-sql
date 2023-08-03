3ef0b802bd8bb4bb797a1e29f04338e414a551c9
Brettly <brettcd@icloud.com>
2023-08-02 19:12:57 -0700

    Consolidate vector and db setup steps into one file -- setup.py.

--- src/data/setup/setup.py
+++ src/data/setup/setup.py
@@ -3,6 +3,14 @@ import pandas as pd
 import os
 import json
 
+from langchain import SQLDatabase
+from langchain.docstore.document import Document
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from sqlalchemy import exc
+
+from vector_setup_functions import get_json, connect_db, prep_chroma_documents, create_chroma_db
 from db_setup_functions import get_filenames, get_table_names, get_column_info, df_text_processing, build_schema_info, convert_df_to_json
 
 #download spider database from this location "https://drive.google.com/uc?export=download&id=1TqleXec_OykOYFREKKtschzY29dUcVAQ"
@@ -37,3 +45,21 @@ with open(save_path+'schema_info.json', 'w') as file:
 print("...Success")
 
 
+
+#### CREATING VECTOR DATABASE FROM SCHEMA INFORMATION ####
+
+#setup embeddings using HuggingFace
+embeddings = HuggingFaceEmbeddings()
+
+#point to json file with schema info
+json_path = '../data/processed/db/schema_info.json'
+
+#point to location with .sqlite files
+data_directory = '../data/raw/spider/database/'
+
+#point to location to save the vector database
+persist_directory = '../data/processed/chromadb/'
+
+schema_docs = prep_chroma_documents(json_path=json_path, db_path=data_directory)
+
+create_chroma_db(docs=schema_docs, persist_dir=persist_directory, embed_func=embeddings)
